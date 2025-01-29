a5f5a6609331e3bca2b6940c0a3b0df9097bf834
Rushain Sovis <rsovis@wiley.com>
2025-01-29 15:00:58 +0530

    test

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ParentAccordionDemo from './components/Accordion/ParentAccordionDemo';
+
+function App() {
+    return (
+        <div>
+            <ParentAccordionDemo />
+        </div>
+    );
+}
+
+export default App;
